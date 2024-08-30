7c95b1f9bbec3b91c9b1bd5bca0937975141bb81
jayasree <jayasreekrishnamurthy60@gmail.com>
2024-08-30 11:53:16 +0530

    Initialize project using Create React App

---
+++ src/Components/stopWatch.jsx
@@ -0,0 +1,11 @@
+import react, { useState } from "react";
+const Stopwatch = () = >{
+    const [time,setTime] = useState(0);
+    const [running, setRunning]- useState(false)
+
+    return(
+      <>
+
+      </>
+    );
+};
